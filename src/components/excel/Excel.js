9f06ef42dde153e70fae8f572305332e84e8e2d3
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-08-13 15:33:44 +0300

    начал класс dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,25 +2,18 @@ import { $ } from "../../core/dom";
 
 export class Excel {
 	constructor(appSelector, options) {
-		this.$appEl = document.querySelector(appSelector);
+		this.$appEl = $(appSelector);
 		this.components = options.components || [];
 	}
 
-	static className = 'excel';
-
 	getRoot() {
-		// const $root = document.createElement('div');
-		// $root.classList.add(this.className);
-		const $root = $.create('div', this.className);
+		const $root = $.create('div', 'excel');
 
 		this.components.forEach(Component => {
-			// const $rootEl = document.createElement('div');
-			// $rootEl.classList.add(Component.className);
 			const $rootEl = $.create('div', Component.className);
-
 			const component = new Component($rootEl);
-			$rootEl.innerHTML = component.toHTML();
 
+			$rootEl.html(component.toHTML());
 			$root.append($rootEl);
 		});
 
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,7 +1,35 @@
-class Dom {}
+class Dom {
+	constructor(selector) {
+		if (typeof selector === 'string') {
+			this.$el = document.querySelector(selector);
+		} else {
+			this.$el = selector;
+		}
+	}
 
-export function $() {
-	return new Dom();
+	html(newHtml) {
+		if (typeof newHtml === 'string') {
+			this.$el.innerHTML = newHtml;
+			return this;
+		}
+
+		return this.$el.innerHTML;
+	}
+
+	clear() {
+		this.html('');
+
+		return this;
+	}
+
+	append(node) {
+		this.$el.appendChild(node.$el);
+		
+		return this;
+	}
+}
+export function $(selector) {
+	return new Dom(selector);
 }
 
 $.create = (tagName, classes) => {
@@ -11,5 +39,5 @@ $.create = (tagName, classes) => {
 		el.classList.add(classes);
 	}
 
-	return el;
+	return $(el);
 };
