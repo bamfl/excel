e876bb0da69e027e254142cf36dc1dad05aca7aa
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-08-17 15:47:05 +0300

    Вынес функции событий в отдельные файлы

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,4 +1,7 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
+import { mousedown } from './table.mousedown';
+import { mousemove } from './table.mousemove';
+import { mouseup } from './table.mouseup';
 import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
@@ -14,7 +17,7 @@ export class Table extends ExcelComponent {
 	}
 
 	toHTML() {
-		return createTable(20);
+		return createTable(1000);
 	}
 
 	onClick() {
@@ -22,59 +25,14 @@ export class Table extends ExcelComponent {
 	}
 
 	onMousedown(event) {
-		this.isClicked = true;
-
-		if (event.target.classList.contains('column-resize')) {
-			this.column = event.target.closest('[data-type="resizable"]');
-			this.columnWidth = parseInt(getComputedStyle(this.column).width);
-			this.positionXStart = event.clientX;
-			this.column.querySelector('.column-resize').style.display = 'block';
-
-			this.getColIndex = (this.column.firstChild.nodeValue.trim().charCodeAt() - 64);
-			this.cells = document.querySelectorAll(`[data-col="${this.getColIndex}"]`);
-			this.cells.forEach(cell => {
-				cell.style.borderRight = '1px solid #88b8ff';
-			});
-
-		} else if (event.target.classList.contains('row-resize')) {
-			this.row = event.target.closest('[data-type="resizable"]');
-			this.rowHeight = parseInt(getComputedStyle(this.row).height);
-			this.positionYStart = event.clientY;
-			this.row.querySelector('.row-resize-line').style.display = 'block';
-			this.row.querySelector('.row-resize').style.display = 'block';
-		}
+		mousedown.call(this, event);
 	}
 
 	onMousemove(event) {
-		if (this.isClicked) {
-			if (this.column) {
-				const resizeX = this.positionXStart - event.clientX;
-				this.column.style.width = this.columnWidth - resizeX + 'px';
-
-				this.cells.forEach(cell => {
-					cell.style.width = this.columnWidth - resizeX + 'px';
-				});
-
-			} else if (this.row) {
-				const resizeY = this.positionYStart - event.clientY;	
-				this.row.style.height = this.rowHeight - resizeY + 'px';
-			}
-		}
+		mousemove.call(this, event);
 	}
 
 	onMouseup() {
-		this.isClicked = false;
-
-		if (this.row) {
-			this.row.querySelector('.row-resize-line').style.display = '';
-			this.row.querySelector('.row-resize').style.display = '';
-			this.row = null;
-		} else if (this.column) {
-			this.cells.forEach(cell => {
-				cell.style.borderRight = '';
-			});
-			this.column.querySelector('.column-resize').style.display = '';
-			this.column = null;
-		}
+		mouseup.call(this);
 	}
 }
---
+++ src/components/table/table.mousedown.js
@@ -0,0 +1,21 @@
+export function mousedown(event) {
+  this.isClicked = true;
+
+  if (event.target.classList.contains('column-resize')) {
+    this.column = event.target.closest('[data-type="resizable"]');
+    this.columnWidth = parseInt(getComputedStyle(this.column).width);
+    this.positionXStart = event.clientX;
+    this.column.querySelector('.column-resize').style.display = 'block';
+    this.getColIndex = this.column.firstChild.nodeValue.trim().charCodeAt() - 64;
+    this.cells = document.querySelectorAll(`[data-col="${this.getColIndex}"]`);
+    this.cells.forEach((cell) => {
+      cell.style.borderRight = '1px solid #88b8ff';
+    });
+  } else if (event.target.classList.contains('row-resize')) {
+    this.row = event.target.closest('[data-type="resizable"]');
+    this.rowHeight = parseInt(getComputedStyle(this.row).height);
+    this.positionYStart = event.clientY;
+    this.row.querySelector('.row-resize-line').style.display = 'block';
+    this.row.querySelector('.row-resize').style.display = 'block';
+  }
+}
